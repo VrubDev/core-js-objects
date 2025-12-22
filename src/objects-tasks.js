af96cd0a0bff4bea3c63779bc91dcd701c3c0b0d
Victoria Vrublevskaya <vivrubl16@gmail.com>
2025-12-22 14:41:54 +0400

    feat: implement fromJSON function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -190,8 +190,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const data = JSON.parse(json);
+  const instance = Object.create(proto);
+  return Object.assign(instance, data);
 }
 
 /**
